7887010f5a1dc1d0a532c254021bbbb4d2eef5a7
acristoni <adonai_cn@hotmail.com>
2024-05-31 13:43:59 -0300

    :new: - search pokemon

--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,60 @@
 <template>
-    <v-container>
-      <v-row>
-        <template v-for="(pokemon) in pokemons" :key="index">  
-          <v-col
-            cols="6"
-            md="3"
-          >
-            <PokemonResumeCard 
-              :name="pokemon.name" 
-              :url="pokemon.url"
-            />
-          </v-col>
-        </template>
-      </v-row>
-    </v-container>
+  <v-container class="template-container">
+    <div
+      style="width: 100%; display: flex; justify-content: center; padding: 4;"
+    >
+      <v-text-field
+        v-model="searchTerm"
+        label="Procure um Pokemon"
+        variant="solo"
+        @keyup.enter="searchPokemon"
+      ></v-text-field>
+    </div>
+    <v-row>
+      <template v-for="(pokemon) in pokemons" :key="index">  
+        <v-col
+          cols="6"
+          md="3"
+        >
+          <PokemonResumeCard 
+            :name="pokemon.name" 
+            :url="pokemon.url"
+          />
+        </v-col>
+      </template>
+    </v-row>
+    <div
+      style="width: 100%; display: flex; justify-content: center;"
+    >
+      <v-btn 
+        class="mt-4"
+        @click="handleGetMorePokemonsButton"
+      >Ver mais Pokemons</v-btn>
+    </div>
+  </v-container>
 </template>
   
 <script setup>  
-    const pokemons = ref([])      
-    const { data, pending, error } = await useFetch('https://pokeapi.co/api/v2/pokemon',{});
-    pokemons.value = data.value.results;
+  const pokemons = ref([]);    
+  const nextUrl = ref(null);
+  const searchTerm = ref('');
+
+  const { data, pending, error } = await useFetch('https://pokeapi.co/api/v2/pokemon',{});
+  pokemons.value = data.value.results;
+  nextUrl.value = data.value.next;
+
+  const handleGetMorePokemonsButton = async () => {
+    console.log('Clicou no botão');
+    if (nextUrl.value) {
+      const { data: newData } = await useFetch(nextUrl.value,{});
+      pokemons.value = [...pokemons.value, ...newData.value.results];
+      nextUrl.value = newData.value.next;
+    }
+  }
+
+  const searchPokemon = () => {
+    if (searchTerm.value) {
+      navigateTo(`/details/${searchTerm.value}`);
+    }
+  } 
 </script>      
