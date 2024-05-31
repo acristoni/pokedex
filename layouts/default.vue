2a31e946da1e1c0e06a01b1dc06028c76948c64c
acristoni <adonai_cn@hotmail.com>
2024-05-31 08:08:10 -0300

    :new: - layout home

---
+++ layouts/default.vue
@@ -0,0 +1,30 @@
+<template>
+    <v-app id="inspire">  
+      <v-app-bar>  
+        <v-app-bar-title>Pokedex</v-app-bar-title>
+      </v-app-bar>
+  
+      <v-navigation-drawer
+        v-model="drawer"
+        temporary
+      >
+        <!--  -->
+      </v-navigation-drawer>
+  
+      <v-main class="bg-grey-lighten-2">
+        <slot></slot>
+      </v-main>
+    </v-app>
+  </template>
+  
+  <script setup>
+    import { ref } from 'vue'
+  
+    const drawer = ref(null)
+  </script>
+  
+  <script>
+    export default {
+      data: () => ({ drawer: null }),
+    }
+  </script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,18 @@
 <template>
-    <Header />
-    <div>
-        Página inicial
-    </div>
-    <Footer />
+    <v-container>
+        <v-row>
+        <template v-for="n in 4" :key="n">  
+            <v-col
+            v-for="j in 6"
+            :key="`${n}${j}`"
+            cols="6"
+            md="2"
+            >
+            <v-sheet height="150"></v-sheet>
+            </v-col>
+        </template>
+        </v-row>
+    </v-container>
 </template>
 
 <script setup>
